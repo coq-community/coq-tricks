6fa03a8839c3a2bbb40fff9b8964c2f5efaf8ee1
Tej Chajed <tchajed@mit.edu>
2016-11-23 16:34:48 -0600

    Add some example Search queries

---
+++ Search.v
@@ -0,0 +1,13 @@
+Require Import Arith.
+
+Search "mod" "/".
+
+Search (_ + ?a - ?a).
+
+Search sumbool (@eq bool _ _).
+
+Search ({_=_} + {_<>_}).
+
+Search list outside List.
+
+Search plus inside List.
