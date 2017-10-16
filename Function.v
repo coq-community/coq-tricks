e571626431eca7739c01091571ad0d65e63147ae
foreverbell <dql.foreverbell@gmail.com>
2017-10-16 10:05:46 +0800

    Fix Function.v

--- Function.v
+++ Function.v
@@ -1,32 +1,33 @@
 Require Import Recdef Div2 Omega FunctionalExtensionality.
 
-(** [foo] and [bar] count the number of digits in binary representation. *)
+(** Counting the number of digits in binary representation. *)
 
-(** Error: Cannot guess decreasing argument of fix. *)
-Fail Fixpoint foo (n : nat) (b : bool) : nat :=
+(** Show directly defining this function with Fixpoint is not acceptable.
+    Error: Cannot guess decreasing argument of fix. *)
+Fail Fixpoint attempt_1 (n r : nat) : nat :=
   match n with
-  | 0 => 42
-  | S n' => if b then foo n false else foo n' true
+  | 0 => r
+  | _ => attempt_1 (Nat.div2 n) (r + 1)
   end.
 
 (** function with decreasing measure. *)
-Function foo (n r : nat) {measure (fun x => x) n} : nat :=
+Function attempt_2 (n r : nat) {measure (fun x => x) n} : nat :=
   match n with
   | 0 => r
-  | _ => foo (Nat.div2 n) (r + 1)
+  | _ => attempt_2 (Nat.div2 n) (r + 1)
   end.
 Proof.
   intros. apply lt_div2. omega.
 Defined.
 
-Compute foo 1024 0.
+Compute attempt_2 1024 0.
 
 (** function with well-founded relation. *)
 Print lt.
-Function bar (n r : nat) {wf lt n} : nat :=
+Function attempt_3 (n r : nat) {wf lt n} : nat :=
   match n with
   | 0 => r
-  | _ => bar (Nat.div2 n) (r + 1)
+  | _ => attempt_3 (Nat.div2 n) (r + 1)
   end.
 Proof.
   + intros. apply lt_div2. omega.
@@ -35,13 +36,13 @@ Defined.
 
 (** they are totally same. *)
 Goal
-  foo = bar.
+  attempt_2 8 0 = 4.
 Proof.
-  extensionality n.
-  extensionality r.
-  induction n using lt_wf_ind.
   (** use [xx_equation] to expand the recursive function. *)
-  rewrite (foo_equation n r).
-  rewrite (bar_equation n r).
-  destruct n; auto.
+  rewrite attempt_2_equation; simpl.
+  rewrite attempt_2_equation; simpl.
+  rewrite attempt_2_equation; simpl.
+  rewrite attempt_2_equation; simpl.
+  rewrite attempt_2_equation; simpl.
+  reflexivity.
 Qed.
