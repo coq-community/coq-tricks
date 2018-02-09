3cc6eba61a1bdb7f6569c482e53d5c4a83ad4b4d
Tej Chajed <tchajed@mit.edu>
2018-02-09 09:37:27 -0500

    Add an example of using [Fix] directly.

--- Function.v
+++ Function.v
@@ -53,3 +53,86 @@ Theorem attempts_2_and_3_are_the_same :
 Proof.
   reflexivity.
 Qed.
+
+(** Now we'll redo the above using the standard library's [Fix] combinator,
+which provides general well-founded recursion.
+
+ See CPDT's chapter http://adam.chlipala.net/cpdt/html/GeneralRec.html for an
+ excellent overview, including a better description of how this approach itself
+ works under the hood. *)
+Check Fix.
+(*
+Fix
+     : forall (A : Type) (R : A -> A -> Prop),
+       well_founded R ->
+       forall P : A -> Type,
+       (forall x : A, (forall y : A, R y x -> P y) -> P x) ->
+       forall x : A, P x
+
+To read this, start at the bottom: we're writing a function of type [forall (x:A), P
+x]. The recursion is over [A] and decreases the relation [R], which must be
+well-founded (think of [A = nat] and [R = lt] as examples). The body of the
+function has the type
+
+[forall (x:A) (F: forall (y:A), R y x -> P y), P x]
+
+[x] is simply the argument to the body. The second argument is what I've called
+[F] here: think of this as being the function being defined itself, in order to
+support recursion. The catch is in its signature: instead of just taking [y:A],
+it also takes [R y x]; that is, in order to call [F] recursively, you must pass
+a proof that the argument used is smaller than the outer argument [x]. Because
+[R] is well-founded, eventually this body must stop calling itself recursively.
+ *)
+
+Lemma div2_smaller : forall n n',
+    n = S n' ->
+    Nat.div2 n < n.
+Proof.
+  destruct n; intros.
+  discriminate.
+  apply lt_div2; omega.
+Qed.
+
+Definition numdigits (n: nat) : nat.
+  refine
+    (Fix lt_wf (fun _ => nat)
+         (fun n (numdigits: forall (n':nat), n' < n -> nat) =>
+            (* unfortunately we need to convoy a proof to keep track of the
+            value of [n] when proving we decrease the argument to [numdigits] *)
+            match n as n0 return (n = n0 -> nat) with
+            | 0 => fun _ => 0
+            | S n' => fun H => 1 + numdigits (Nat.div2 n) _
+            end eq_refl) n).
+  eapply div2_smaller; eauto.
+Defined.
+
+(* We need to provide a wrapper around the standard library's [Fix_eq], which
+describes the computational behavior of a [Fix] as being the same as a one-step
+unfolding. However, the theorem also requires one to prove the body is unable to
+distinguish between [F] arguments that are extensionally equal; this is true of
+any Gallina code but can't be proven within Coq once and for all. *)
+Theorem numdigits_eq : forall n,
+    numdigits n = match n with
+                  | 0 => 0
+                  | _ => 1 + numdigits (Nat.div2 n)
+                  end.
+Proof.
+  intros.
+  match goal with
+  | [ |- ?lhs = _ ] =>
+    match eval unfold numdigits in lhs with
+    | Fix ?wf ?P ?F _ =>
+      rewrite (Fix_eq wf P F)
+    end
+  end.
+  destruct n; reflexivity.
+
+  intros.
+  destruct x; auto.
+Qed.
+
+Example numdigits_5 : numdigits 8 = 4.
+Proof.
+  repeat (rewrite numdigits_eq; cbn [Nat.div2 plus]).
+  reflexivity.
+Qed.
