51b7911c749273580eee1c424866d5a53185078c
Tej Chajed <tchajed@mit.edu>
2020-11-19 10:07:55 -0600

    Update instance generalization example

--- src/InstanceGeneralization.v
+++ src/InstanceGeneralization.v
@@ -4,6 +4,10 @@ coq-std++ authors (Robbert Krebbers et al) consider this a bug, and I agree. At
 the very least generalizing variables in instances should be optional. *)
 Class Trivial (A:Type) := trivial : unit.
 
+(* this behavior has been fixed in Coq 8.13; this flag enables the old
+surprising behavior *)
+Set Instance Generalized Output.
+
 Generalizable Variable A.
 Instance generic_trivial : Trivial A.
 intros A. (* instance quantifies over A *)
