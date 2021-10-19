3ce37ceebe698087d1420cee6886b7ba7baea488
Tej Chajed <tchajed@mit.edu>
2021-10-19 07:52:50 -0400

    Remove ssreflect Search demo
    ssreflect Search no longer hijacks Coq's Search

--- src/Search.v
+++ src/Search.v
@@ -21,8 +21,6 @@ works without decidable equality but requires the axiom of proof irrelevance -
 the search will not find it if ProofIrrelevance has not been Require'd. *)
 Search (existT ?P ?x _ = existT ?P ?x _).
 
-Search list outside List.
-
 Search plus inside List.
 
 (* searches use the name of the theorem if the string is an identifier *)
@@ -34,29 +32,3 @@ in the lemma name; to search for the notation search for the keyword with
 Search "'mod'" 2.
 (* the above search would probably be better done like this: *)
 Search (_ mod 2).
-
-(* note that requiring ssreflect in _any transitive dependency_ hijacks the
-built-in Search and replaces it with ssreflect, which is similar but slightly
-different.
- *)
-Require ssreflect.
-(* ssreflect Search has only one improvement over Coq's builtin Search and a
-serious footgun.
-
-The improvement is that searches for a notation also return the notation's
-definition, so you don't need a separate [Locate].
-
-The footgun is that the first component of an ssreflect search is special and
-only matches within the "conclusion" (the tail of a sequence of [forall] and
-[->]). To disable this, you need to pass a wildcard for the first position. *)
-
-(* for example, let's try to find the function [nth_error : list A -> nat ->
-option A], knowing only that we want some function to lookup in a list. *)
-
-(* this is a pretty sensible search *)
-Search list nat option.
-(* ... but it returns a warning [Warning: Listing only lemmas with conclusion
-matching (list _)], which is of course useless. *)
-
-(* instead, you need to do this: *)
-Search _ list nat option.
